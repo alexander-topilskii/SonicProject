802f0e99e851211d96b07d43f99e6498af2d9794
alexander-topilskii <alexander.topilskii@gmail.com>
2024-04-27 22:08:55 +0300

    some awesome:

--- sonic_ui/src/main/java/com/ato/sonic_ui/base/button/button_row.kt
+++ sonic_ui/src/main/java/com/ato/sonic_ui/base/button/button_row.kt
@@ -3,11 +3,15 @@ package com.ato.sonic_ui.base.button
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ato.ui_state.base.button.UiButton
 import com.ato.ui_state.base.button.UiButtonsRow
 
@@ -20,9 +24,12 @@ fun UiButtonsRow.Display(onClick: (UiButton) -> Unit, modifier: Modifier = Modif
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
+        Spacer(modifier = Modifier.width(16.dp))
         buttons.forEach {
             it.Display(onClick = { onClick.invoke(it) })
+            Spacer(modifier = Modifier.width(4.dp))
         }
+        Spacer(modifier = Modifier.width(16.dp))
     }
 }
 
