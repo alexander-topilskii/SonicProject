cac609a1489b1da9a6e2fcc5fa080d9974903310
alexander-topilskii <alexander.topilskii@gmail.com>
2024-06-09 12:34:12 +0300

    some awesome:some awesome

--- sonic_ui/src/main/java/com/ato/ui_state/ikirag/IkiragData.kt
+++ sonic_ui/src/main/java/com/ato/ui_state/ikirag/IkiragData.kt
@@ -8,6 +8,7 @@ import androidx.room.Index
 import androidx.room.Junction
 import androidx.room.PrimaryKey
 import androidx.room.Relation
+import com.ato.ui_state.Ui
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -16,8 +17,8 @@ data class IkiragData(
     val text: String,
     val isLiked: Boolean? = null,
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0L
-)
+    override val id: Long = 0L
+) : Ui
 
 @Serializable
 @Entity(
