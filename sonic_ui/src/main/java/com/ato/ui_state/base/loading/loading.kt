b0e42e9117e4de7c0c909a7fc55c96879a831cd9
alexander-topilskii <alexander.topilskii@gmail.com>
2024-06-16 09:43:38 +0300

    some awesome:

---
+++ sonic_ui/src/main/java/com/ato/ui_state/base/loading/loading.kt
@@ -0,0 +1,42 @@
+package com.ato.ui_state.base.loading
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Loading(modifier: Modifier = Modifier) {
+    val animatedProgress = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        animatedProgress.animateTo(
+            targetValue = 2f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 1500, easing = LinearEasing),
+            ),
+        )
+    }
+
+
+    Canvas(modifier = modifier.fillMaxSize()) {
+        val x: Float = this.center.x * animatedProgress.value
+        val offset = Offset(x, this.center.y)
+        drawCircle(
+            Color.Black,
+            radius = 20f,
+            center = offset
+        )
+    }
+}
+
