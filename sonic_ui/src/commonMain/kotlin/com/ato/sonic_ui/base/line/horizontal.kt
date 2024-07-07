e1850ac1fdeb309479da213d235867b8892d911a
alexander.topilskii <alexander.topilskii@gmail.com>
2024-07-07 09:26:53 +0300

    some awesome: add settings page

---
+++ sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/line/horizontal.kt
@@ -0,0 +1,32 @@
+package com.ato.sonic_ui.base.line
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun HorizontalSpaceLine(
+    spaceStart: Dp,
+    spaceEnd: Dp,
+    lineColor: Color,
+    line: Dp = 1.dp,
+) {
+    Spacer(
+        modifier = Modifier.height(spaceStart)
+    )
+    Spacer(
+        modifier = Modifier.height(line)
+            .fillMaxWidth()
+            .background(lineColor)
+    )
+    Spacer(
+        modifier = Modifier.height(spaceEnd)
+    )
+}
