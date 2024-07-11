1f5bfab9fb32fc935d4137d2dc1ca2eda2ca0844
alexander.topilskii <alexander.topilskii@gmail.com>
2024-07-11 17:20:31 +0300

    some awesome: ad accept and decline

--- sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/wishlist/PersonCard.kt
+++ sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/wishlist/PersonCard.kt
@@ -1,8 +1,9 @@
 package com.ato.sonic_ui.wishlist
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.ato.sonic_ui.base.button.DisplayButton
+import com.ato.ui_state.base.button.UiButton
 
 @Composable
 fun PersonCard(
@@ -39,5 +42,48 @@ fun PersonCard(
                 .padding(horizontal = 8.dp)
                 .padding(bottom = 8.dp)
         )
+    }
+}
+
+@Composable
+fun PersonCardButtons(
+    name: String,
+    nick: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    acceptButton: UiButton,
+    declineButton: UiButton,
+    onDecline: () -> Unit,
+    onBlock: () -> Unit,
+) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        onClick = onClick
+    ) {
+        Text(
+            text = name,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .padding(top = 8.dp)
+        )
+        Spacer(Modifier.height(2.dp))
+        Text(
+            text = "@${nick}",
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 8.dp)
+        )
+        Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+            DisplayButton(
+                state = acceptButton,
+                onClick = onDecline
+            )
+            DisplayButton(
+                state = declineButton,
+                onClick = onBlock
+            )
+        }
     }
 }
