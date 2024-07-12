b69fc59f5b4736216b9045b432b3eab10b792fa2
alexander.topilskii <alexander.topilskii@gmail.com>
2024-07-12 17:42:32 +0300

    some awesome: google auth

--- sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/text/text.kt
+++ sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/text/text.kt
@@ -2,12 +2,17 @@ package com.ato.sonic_ui.base.text
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.ato.ui_state.base.text.UiSimpleText
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun DisplayText(
-    state: UiSimpleText
+    state: UiSimpleText,
+    modifier: Modifier = Modifier
 ) {
-    Text(text = stringResource(state.text))
+    Text(
+        text = stringResource(state.text),
+        modifier = modifier
+    )
 }
