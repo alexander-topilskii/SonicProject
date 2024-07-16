274908904a64a26b965a2d7e0675fb585468075b
alexander.topilskii <alexander.topilskii@gmail.com>
2024-07-16 23:23:15 +0300

    some awesome: deeplinks: add wish

--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/DeepLinkData.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/DeepLinkData.kt
@@ -1,25 +1,41 @@
 package com.ato.ui_state.wishlist
 
 import io.ktor.http.Url
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class DeepLinkData(
     val scheme: String?,
     val host: String?,
     val pathSegments: List<String>,
     val userId: String?,
-    val wishId: String?
+    val wishId: String?,
+    val wishName: String?,
+    val wishUrl: String?,
+    val wishDescription: String?,
 ) {
     companion object {
-        const val APP : String = "app"
-        const val FRIENDS : String = "friends"
-        const val WISHES : String = "wishes"
-        const val SETTINGS : String = "settings"
-        const val EVENTS : String = "events"
+        const val APP: String = "app"
+        const val FRIENDS: String = "friends"
+        const val WISHES: String = "wishes"
+        const val ADD_WISH: String = "add_wish"
+        const val SETTINGS: String = "settings"
+        const val EVENTS: String = "events"
+
+        const val PARAM_USER_ID = "userId"
+        const val PARAM_WISH_ID = "wishId"
+        const val PARAM_WISH_NAME = "wishName"
+        const val PARAM_WISH_URL = "wishUrl"
+        const val PARAM_WISH_DESCRIPTION = "wishDescription"
 
         fun fromUrl(url: Url): DeepLinkData? {
             val pathSegments = url.pathSegments.filter { it.isNotEmpty() }
-            val userId = url.parameters["userId"]
-            val wishId = url.parameters["wishId"]
+            val userId = url.parameters[PARAM_USER_ID]
+            val wishId = url.parameters[PARAM_WISH_ID]
+
+            val wishName = url.parameters[PARAM_WISH_NAME]
+            val wishUrl = url.parameters[PARAM_WISH_URL]
+            val wishDescription = url.parameters[PARAM_WISH_DESCRIPTION]
 
             return if (pathSegments.first() == APP) {
                 DeepLinkData(
@@ -27,7 +43,10 @@ data class DeepLinkData(
                     host = url.host,
                     pathSegments = pathSegments,
                     userId = userId,
-                    wishId = wishId
+                    wishId = wishId,
+                    wishName = wishName,
+                    wishUrl = wishUrl,
+                    wishDescription = wishDescription
                 ).getTail()
             } else {
                 null
@@ -44,13 +63,16 @@ data class DeepLinkData(
     }
 
     fun getTail(): DeepLinkData? {
-        return if (pathSegments.size > 1) {
+        return if (pathSegments.isNotEmpty()) {
             DeepLinkData(
                 scheme = scheme,
                 host = host,
                 pathSegments = pathSegments.drop(1),
                 userId = userId,
-                wishId = wishId
+                wishId = wishId,
+                wishName = wishName,
+                wishUrl = wishUrl,
+                wishDescription = wishDescription
             )
         } else {
             null
