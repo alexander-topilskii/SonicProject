4ba147279f0de29d6416d2e8128985d16a33666e
alexander.topilskii <alexander.topilskii@gmail.com>
2024-07-16 23:56:59 +0300

    some awesome: deeplinks: add wish from url

--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/DeepLinkData.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/DeepLinkData.kt
@@ -8,11 +8,11 @@ data class DeepLinkData(
     val scheme: String?,
     val host: String?,
     val pathSegments: List<String>,
-    val userId: String?,
-    val wishId: String?,
-    val wishName: String?,
-    val wishUrl: String?,
-    val wishDescription: String?,
+    val userId: String? = null,
+    val wishId: String? = null,
+    val wishName: String? = null,
+    val wishUrl: String? = null,
+    val wishDescription: String? = null,
 ) {
     companion object {
         const val APP: String = "app"
@@ -27,35 +27,59 @@ data class DeepLinkData(
         const val PARAM_WISH_NAME = "wishName"
         const val PARAM_WISH_URL = "wishUrl"
         const val PARAM_WISH_DESCRIPTION = "wishDescription"
+        val addWish = DeepLinkData(
+            scheme = "wishlist",
+            host = "yalito.me",
+            pathSegments = listOf("app", "wish", "add_wish"),
+            userId = null,
+            wishId = null,
+            wishUrl = null
+        )
 
-        fun fromUrl(url: Url): DeepLinkData? {
-            val pathSegments = url.pathSegments.filter { it.isNotEmpty() }
-            val userId = url.parameters[PARAM_USER_ID]
-            val wishId = url.parameters[PARAM_WISH_ID]
+        fun fromUrl(urlString: String): DeepLinkData? {
 
-            val wishName = url.parameters[PARAM_WISH_NAME]
-            val wishUrl = url.parameters[PARAM_WISH_URL]
-            val wishDescription = url.parameters[PARAM_WISH_DESCRIPTION]
+            return if (
+                urlString.startsWith("https://yalito.me/app/") ||
+                urlString.startsWith("wishlist://yalito.me/app/")
+            ) {
+                val url = Url(urlString)
+                val pathSegments = url.pathSegments.filter { it.isNotEmpty() }
+                val userId = url.parameters[PARAM_USER_ID]
+                val wishId = url.parameters[PARAM_WISH_ID]
+                val wishName = url.parameters[PARAM_WISH_NAME]
+                val wishUrl = url.parameters[PARAM_WISH_URL]
+                val wishDescription = url.parameters[PARAM_WISH_DESCRIPTION]
 
-            return if (pathSegments.first() == APP) {
                 DeepLinkData(
                     scheme = url.protocol.name,
                     host = url.host,
                     pathSegments = pathSegments,
                     userId = userId,
                     wishId = wishId,
-                    wishName = wishName,
                     wishUrl = wishUrl,
-                    wishDescription = wishDescription
+                    wishDescription = wishDescription,
+                    wishName = wishName
                 ).getTail()
             } else {
-                null
+                addWish.getTail()
             }
         }
-    }
 
-    fun getPathSegment(index: Int): String? {
-        return if (index in pathSegments.indices) pathSegments[index] else null
+        fun getAddWishWithDescription(sharedUris: List<String>): DeepLinkData? {
+            return addWish.copy(wishDescription = sharedUris.joinToString { "/n" }).getTail()
+        }
+
+        fun getAddWishWithDescription(sharedDescription: String): DeepLinkData? {
+            return addWish.copy(wishDescription = sharedDescription).getTail()
+        }
+
+        fun getAddWishWithName(name: String): DeepLinkData? {
+            return addWish.copy(wishName = name).getTail()
+        }
+
+        fun getAddWishWithLink(url: String): DeepLinkData? {
+            return addWish.copy(wishUrl = url).getTail()
+        }
     }
 
     fun getHead(): String? {
