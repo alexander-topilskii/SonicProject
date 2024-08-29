79b5c798c8cc3c1fbddce4485079a225c05527f2
alexander.topilskii <alexander.topilskii@gmail.com>
2024-08-29 22:47:10 +0300

    some awesome: add delete account

--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistBoard.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistBoard.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class WishlistBoard(
+    @kotlinx.serialization.Transient
     var documentId: String? = null,
     var userDocumentId: String? = null,
     var name: String? = null,
--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistWish.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistWish.kt
@@ -5,12 +5,14 @@ import dev.gitlive.firebase.internal.FirebaseDecoder
 import dev.gitlive.firebase.internal.FirebaseEncoder
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class WishlistWish(
+    @Transient
     var documentId: String? = null,
     var userDocumentId: String? = null,
     @Deprecated("use boardDocumentIds")
