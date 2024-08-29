01dcd4b845d4ac2f106248cbcc7a2a66e4e4237d
alexander.topilskii <alexander.topilskii@gmail.com>
2024-08-29 19:27:52 +0300

    some awesome:

--- sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/text/title.kt
+++ sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/text/title.kt
@@ -2,6 +2,7 @@ package com.ato.sonic_ui.base.text
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -37,7 +39,7 @@ import org.jetbrains.compose.resources.stringResource
 
 
 @Composable
-fun DisplayTitle(
+fun LazyItemScope.DisplayTitle(
     state: UiTitle,
     onClick: () -> Unit = {},
     onBackClicked: (() -> Unit)? = null,
@@ -110,4 +112,150 @@ fun DisplayTitle(
         lineColor = MaterialTheme.colorScheme.primary,
         spaceBottom = 16.dp
     )
+}
+
+@Composable
+fun ColumnScope.DisplayTitle(
+    state: UiTitle,
+    onClick: () -> Unit = {},
+    onBackClicked: (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    Spacer(modifier = Modifier.height(24.dp))
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        val title = if (state.titleFormatArgs == null) {
+            stringResource(state.title)
+        } else {
+            stringResource(state.title, state.titleFormatArgs!!)
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(end = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (onBackClicked != null) {
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(36.dp)
+                        .clip(CircleShape)
+                        .clickable(
+                            onClick = onBackClicked,
+                        )
+                        .padding(8.dp),
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+            } else {
+                Spacer(modifier = Modifier.width(16.dp))
+            }
+
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 36.sp,
+            )
+        }
+
+        state.button?.let {
+            when (it) {
+                is UiButton -> DisplayButton(
+                    state = it,
+                    onClick = onClick,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                )
+                is UiIconButton -> DisplayUiIconButton(
+                    state = it,
+                    onClick = onClick,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                )
+            }
+
+        }
+    }
+    HorizontalSpaceLine(
+        spaceTop = 8.dp,
+        line = 1.dp,
+        lineColor = MaterialTheme.colorScheme.primary,
+        spaceBottom = 16.dp
+    )
+}
+
+@Composable
+fun DisplayAppBarTitle(
+    state: UiTitle,
+    onClick: () -> Unit = {},
+    onBackClicked: (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    Spacer(modifier = Modifier.height(24.dp))
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        val title = if (state.titleFormatArgs == null) {
+            stringResource(state.title)
+        } else {
+            stringResource(state.title, state.titleFormatArgs!!)
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(end = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (onBackClicked != null) {
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(36.dp)
+                        .clip(CircleShape)
+                        .clickable(
+                            onClick = onBackClicked,
+                        )
+                        .padding(8.dp),
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+            } else {
+                Spacer(modifier = Modifier.width(16.dp))
+            }
+
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 36.sp,
+            )
+        }
+
+        state.button?.let {
+            when (it) {
+                is UiButton -> DisplayButton(
+                    state = it,
+                    onClick = onClick,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                )
+                is UiIconButton -> DisplayUiIconButton(
+                    state = it,
+                    onClick = onClick,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                )
+            }
+
+        }
+    }
 }
