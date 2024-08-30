66e30fe27b9d0d9bf0aa3ae0c7cc0292876714c7
alexander.topilskii <alexander.topilskii@gmail.com>
2024-08-30 15:47:15 +0300

    some awesome: edit create account

--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistBoard.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistBoard.kt
@@ -2,13 +2,18 @@ package com.ato.ui_state.wishlist
 
 import dev.gitlive.firebase.firestore.Timestamp
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class WishlistBoard(
-    @kotlinx.serialization.Transient
+    @Transient
     var documentId: String? = null,
     var userDocumentId: String? = null,
     var name: String? = null,
     var wishListIds: List<String>? = emptyList(),
+    var availableForFollowers: Boolean? = null,
+    var availableForFollowing: Boolean? = null,
+    var availableForAll: Boolean? = null,
+    var availableForUserIds: List<String>? = emptyList(),
     var creationDate: Timestamp? = null,
 )
