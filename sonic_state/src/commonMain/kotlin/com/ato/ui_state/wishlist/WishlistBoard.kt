85876f3e3974fd89c20f1700372479e65eefd1bd
alexander.topilskii <alexander.topilskii@gmail.com>
2024-09-07 19:22:06 +0300

    some awesome: rename field

--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistBoard.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistBoard.kt
@@ -10,7 +10,7 @@ data class WishlistBoard(
     var documentId: String? = null,
     var userDocumentId: String? = null,
     var name: String? = null,
-    var wishListIds: List<String>? = null,
+    var wishIds: List<String>? = null,
     var availableForFollowing: Boolean? = null,
     var availableForAll: Boolean? = null,
     var availableForUserIds: List<String>? = null,
