bb89cf144d183040b1155167e33541fe33e6dba0
alexander.topilskii <alexander.topilskii@gmail.com>
2024-09-07 10:01:57 +0300

    some awesome:

--- sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/bottom_bar/bottom_bar_material.kt
+++ sonic_ui/src/commonMain/kotlin/com/ato/sonic_ui/base/bottom_bar/bottom_bar_material.kt
@@ -1,14 +1,19 @@
 package com.ato.sonic_ui.base.bottom_bar
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
@@ -21,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.ato.sonic_ui.base.Display
@@ -31,33 +37,44 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun UiNavBar.Display(onClick: (Int) -> Unit = { }) {
-    NavigationBar(
-        modifier = Modifier
-            .height(64.dp)
-    ) {
-        items.forEachIndexed { index, (uiIconText, text, isSelected) ->
-            NavigationBarItem(
-                selected = isSelected,
-                onClick = {
-                    onClick(index)
-                },
-                icon = {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        uiIconText.Display(
-                            tint = LocalContentColor.current,
-                            selected = isSelected,
-                        )
-                        if (text != null) {
-                            Text(
-                                text = text,
-                                fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal
+    Column {
+        HorizontalDivider(
+            Modifier
+                .fillMaxWidth()
+                .height(1.dp),
+            color = MaterialTheme.colorScheme.outline
+        )
+
+        NavigationBar(
+            tonalElevation = 0.dp,
+            containerColor = MaterialTheme.colorScheme.surface,
+            modifier = Modifier
+                .height(64.dp)
+        ) {
+            items.forEachIndexed { index, (uiIconText, text, isSelected) ->
+                NavigationBarItem(
+                    selected = isSelected,
+                    onClick = {
+                        onClick(index)
+                    },
+                    icon = {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            uiIconText.Display(
+                                tint = LocalContentColor.current,
+                                selected = isSelected,
                             )
+                            if (text != null) {
+                                Text(
+                                    text = text,
+                                    fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal
+                                )
+                            }
                         }
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
