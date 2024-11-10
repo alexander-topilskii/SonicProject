c073aeedae436207b625db8a7b74b2aa887a9b45
alexander.topilskii <alexander.topilskii@gmail.com>
2024-11-10 14:49:15 +0200

    some awesome: ADD NICE board

--- sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistToggle.kt
+++ sonic_state/src/commonMain/kotlin/com/ato/ui_state/wishlist/WishlistToggle.kt
@@ -4,8 +4,9 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
 @Serializable
-data class WishlistSettings(
+data class WishlistToggle(
     @Transient
     var documentId: String? = null,
-    val skipName: Boolean? = null
+    val skipName: Boolean? = null,
+    val niceBoardList: Boolean? = null
 )
